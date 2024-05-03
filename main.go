b908cb2dd12540cbc85da37144a125673b39fc81
Akashi23 <dan.zh.1999@gmail.com>
2024-05-03 23:16:14 +0500

    Refactor main.go to gracefully shutdown the server

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
 	ConnectDatabase()
-
 	e := InitRouter()
-	e.Logger.Fatal(e.Start(":8000"))
+	gracefulShutdown(e)
+}
+
+func gracefulShutdown(e *echo.Echo) {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Start server
+	go func() {
+		if err := e.Start(":8000"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
